1ede5056f7afe252362b864483f900fb65e3c9b0
Satyanarayana Padma <ashokkumarchintala@gmail.com>
2016-05-17 00:31:44 +0530

    User 1: Initial commit by First User.........

---
+++ app/src/main/java/indus/myapp/com/MainActivity.java
@@ -0,0 +1,15 @@
+package indus.myapp.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initial Commit by USER : 1 (First User)
+    }
+}
